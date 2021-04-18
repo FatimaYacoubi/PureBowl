dd58b7b1d07d0c0bb00cca7e1c455d86401c1df1
Boujmiln <61500175+Boujmiln@users.noreply.github.com>
2021-04-18 15:22:58 +0200

    Update config.php

--- PureBowl/config.php
+++ PureBowl/config.php
@@ -1,8 +1,31 @@
-<?php     
+<?php    
+
+  class config {
+    private static $pdo = NULL;
+
+    public static function getConnexion() {
+      if (!isset(self::$pdo)) {
+        try{
+          self::$pdo = new PDO('mysql:host=localhost;dbname=webprojet', 'root', '',
+          [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+          
+        }catch(Exception $e){
+          die('Erreur: '.$e->getMessage());
+        }
+      }
+      return self::$pdo;
+    }
+  }
+
+
+/* 
 $databaseHost = 'localhost';
-$databaseName = 'purebowl';
+$databaseName = 'webprojet';
 $databaseUsername = 'root';
 $databasePassword = '';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);  */
  ?>
