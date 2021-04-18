66db72db23f332cbb0d3659d3883d9fa74fc3797
Wala_Alimi <wala.alimi@esprit.tn>
2021-04-18 14:36:04 +0100

    updated config + index + affichercommande

--- PureBowl/config.php
+++ PureBowl/config.php
@@ -1,8 +1,21 @@
-<?php     
-$databaseHost = 'localhost';
-$databaseName = 'purebowl';
-$databaseUsername = 'root';
-$databasePassword = '';
+<?php
+  class config {
+    private static $pdo = NULL;
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
- ?>
+    public static function getConnexion() {
+      if (!isset(self::$pdo)) {
+        try{
+          self::$pdo = new PDO('mysql:host=localhost;dbname=webprojet', 'root', '',
+          [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+          
+        }catch(Exception $e){
+          die('Erreur: '.$e->getMessage());
+        }
+      }
+      return self::$pdo;
+    }
+  }
+?>
