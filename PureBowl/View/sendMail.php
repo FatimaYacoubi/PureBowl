781eb4ff2f4ad4734e2591cfec2549e930abb75f
wala <wala.alimi@esprit.tn>
2021-05-04 01:12:12 -0700

    mail+tuto+pdf+dossiers css

--- PureBowl/View/sendMail.php
+++ PureBowl/View/sendMail.php
@@ -1,49 +1,154 @@
+<?php
+include "../Controller/tutoC.php";
 
-<?PHP
-	include "../Controller/DishC.php";
+$tutoName1 = $_GET['dish'];
+$tutoName2 = $_GET['duration'];
+$tutoName3 = $_GET['ingredients'];
+$tutoName4 = $_GET['steps'];
 
-	$dish=new DishC();
-	$listDishes=$dish->displayDish();
 
+if (isset($_POST['ajax'])) {
+$to = $_POST['to'];
+$subject = $_POST['sub'];
+$msg = $_POST['msg'];
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: ".$_POST['from']."<".$_POST['from'].">";
+
+
+$send = mail($to,$subject,$msg,$headers);
+
+if ($send)
+{
+	echo "<p id='success'>✔️  $to</p>";
+}else{
+	echo "<p id='error'>❌  $to</p>";
+}
+exit();
+}
 ?>
 <!DOCTYPE html>
-<html lang="en"><!-- Basic -->
+<html lang="en">
 <head>
-	<meta charset="utf-8">
+<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">   
-   
+
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
+
      <!-- Site Metas -->
-    <title>Pure Bowl</title>  
+    <title>Mail</title>  
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Site Icons -->
-    <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../images/favicon.ico" type="../image/x-icon">
     <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">    
 	<!-- Site CSS -->
     <link rel="stylesheet" href="../css/style.css">    
+	<!-- Pickadate CSS -->
+    <link rel="stylesheet" href="../css/classic.css">    
+	<link rel="stylesheet" href="../css/classic.date.css">    
+	<link rel="stylesheet" href="../css/classic.time.css">    
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="../css/responsive.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../css/custom.css">
 
-    <!--[if lt IE 9]>-->
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <!--[endif]-->
+	<style>
 
+	.main{
+		max-width: 768px;
+		margin: 0 auto;
+	}
+	#title{
+		color: ;
+	    text-shadow: 0 0 20px grey;
+		text-align: center;
+		font-family: Montserrat;
+	}
+	input[type="text"]{
+		background-color: #E2E5DE;
+		box-shadow: 0 0 11px 0px white;
+		height: 40px;
+		width: 47%;
+		border: #FFE4C4;
+		border-radius: 4px;
+		padding: 15px;
+		margin: 1%;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		color: brown;
+		font-family: Montserrat;
+		font-size: 14px;
+	}
+	input[type="text"]:hover{
+		box-shadow: 0 0 11px 0px #DEB887;
+	}
+	#sub{
+		width: 30%;
+	}
+	textarea{
+		background-color: #E2E5DE;
+		box-shadow: 0 0 11px 0px white;
+		height: 300px;
+    	width: 47%;
+    	max-width: 49%;
+		border: none;
+		border-radius: 4px;
+		padding: 15px;
+		margin: 1%;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		color: brown;
+		font-family: Montserrat;
+		font-size: 14px;
+	}
+	textarea:hover{
+		box-shadow: 0 0 11px 0px #DEB887;
+	}
+	#btn{
+		background-color: #D4B996FF;
+
+		width: 30%;
+		height: 40px;
+	    margin-left: 5px;
+		margin-bottom: 40px;
+		color: grey;
+		border: none;
+		border-radius: 4px;
+		font-family: Montserrat;
+		font-size: 18px;
+		font-weight: bold;
+		letter-spacing: 1px;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		cursor: pointer;
+	}
+	#btn:hover{
+		color: brown;
+	}
+	#success{
+		font-family: Montserrat;
+		color: brown;
+	}
+	#error{
+		font-family: Montserrat;
+		color: white;
+	}
+	</style>
 </head>
 
 <body>
-	<!-- Start header -->
-	<header class="top-navbar">
+	      	<!-- Start header -->
+			  <header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="index.html">
@@ -55,118 +160,122 @@
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item"><a class="nav-link" href="../index.html">Home</a></li>
-						<li class="nav-item active"><a class="nav-link" href="View/menu.php">Menu</a></li>
+						<li class="nav-item active "><a class="nav-link" href="../menu.php">Menu</a></li>
 						<li class="nav-item"><a class="nav-link" href="../about.html">About</a></li>
-						<li class="nav-item dropdown">
+						<li class="nav-item  dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="../reservation.html">Reservation</a>
+								<a class="dropdown-item" href="../stuff.html">Stuff</a>
+								<a class="dropdown-item" href="../gallery.html">Gallery</a>
+							</div>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Offres</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
 								<a class="dropdown-item" href="../offre.html">Offre</a>
-								<a class="dropdown-item" href="offre.html">Promotion</a>
+								<a class="dropdown-item" href="../offre.html">Promotion</a>
 								</div>
 							</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="blog.html">blog</a>
-								<a class="dropdown-item" href="blog-details.html">blog Single</a>
+								<a class="dropdown-item" href="../blog.html">blog</a>
+								<a class="dropdown-item" href="../blog-details.html">blog Single</a>
 							</div>
 						</li>
-						<li class="nav-item"><a class="nav-link" href="affichercommande.php">My orders</a></li>
-						<li class="nav-item"><a class="nav-link" href="../gift.html">Gift</a></li>
+						<li class="nav-item"><a class="nav-link" href="../contact.html">Contact</a></li>
+						<li class="nav-item  "><a class="nav-link" href="../reclamation.html">Reclamation</a></li>
+						<li class="nav-item  "><a class="nav-link" href="../gift.html">Gift</a></li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Sign in</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="login.html">As an administrator</a>
-								<a class="dropdown-item" href="blog-details.html">As a client</a>
-
+								<a class="dropdown-item" href="../login.html">As an administrator</a>
+								<a class="dropdown-item" href="../blog-details.html">As a client</a>
 							</div>
 						</li>
-
 					</ul>
 				</div>
 			</div>
 		</nav>
 	</header>
-	<!-- End header -->
-	
-	<!-- Start All Pages -->
-	<div class="all-page-title page-breadcrumb">
+		<!-- Start All Pages -->
+		<div class="all-page-title page-breadcrumb">
 		<div class="container text-center">
 			<div class="row">
 				<div class="col-lg-12">
-					<h1>Special Menu</h1>
+					<h1>Write your email</h1>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- End All Pages -->
-	
-	<!-- Start Menu -->
-	<div class="menu-box">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="heading-title text-center">
-						<h2>Special Menu</h2>
-						<p>Our nutritious food and drink restores inner and outer vitality.</p>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="special-menu text-center">
-						<div class="button-group filter-button-group">
-							<button class="active" data-filter="*">All</button>
-							<button data-filter=".drinks">Drinks</button>
-							<button data-filter=".lunch">Lunch</button>
-							<button data-filter=".dinner">Dinner</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row special-list">
-				<?PHP
-				foreach($listDishes as $dish){ 
-					?>
-						<div class="col-lg-4 col-md-10 special-grid lunch">
-							<div class="gallery-single fix">
-								<img src="../images/img-01.jpg" class="img-fluid" alt="Image">
-								<div class="why-text">
-								<pre> <h4>  <?PHP echo $dish['name']; echo('                '); echo $dish['price']; ?> </h4> </pre>
-									<p><?PHP echo $dish['ingredients']; ?></p>
-									<h4> <a class="btn btn-lg btn-circle btn-outline-new-black" href="../couscous.html">Get it</a>        
-										<a class="btn btn-lg btn-circle btn-outline-new-black" href="displayTuto.php?dish=<?php echo $dish['name'] ?>">Tuto</a> </h4> 
-								</div>
-							</div>
-						</div>
+<form action="" method="post">
+<div class="main" style="margin-top: 100px;">
+<h1>Send your Mail</h1>
+	<div>
+		<input type="text" name="from" id="from"   value="purebowlcontact@gmail.com" readonly required autofocus>
 
-							<?PHP
-				}
-				?>		
-			</div>
-	
-			</div>
-		</div>
+	</div><br>
+	<div>
+		<input type="text" name="sub" id="sub"  value="Recipe Detail" readonly required autofocus>
+	</div><br>
+	<div>
+		<input type="text" name="to" id="to" placeholder="to ..( write an email )">
+	</div><br>
+	<div>
+		<textarea name="msg" id="msg" placeholder="Write your message here ... ">
+
+		NAME:
+		<?PHP echo $tutoName1; ?>	
+
+		DURATION:
+		<?PHP echo $tutoName2; ?>
+
+
+		INGREDIENTS:
+		<?PHP echo $tutoName3; ?>
+
+
+		STEPS:
+		<?PHP echo $tutoName4; ?>
+
+
+		</textarea>
+
 	</div>
-	<!-- End Menu -->
-	
-	<!-- Start QT -->
-	<div class="qt-box qt-background">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 ml-auto mr-auto text-left">
-					<p class="lead ">
-						" If you're not the one cooking, stay out of the way and compliment the chef. "
-					</p>
-					<span class="lead">Michael Strahan</span>
-				</div>
-			</div>
-		</div>
+	<div><br><br>
+		<button id="btn" onclick="return false">SEND</button>
 	</div>
-	<!-- End QT -->
-	
+	<div id="result"></div>
+</div>
+</form>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$("#btn").on('click',function(){
+		var mailist = $("#to").val().split("\n");
+		var tmailist =  mailist.length;
+		for (var current = 0; current < tmailist; current++) {
+		var from = $("#from").val();
+		var name = $("#name").val();
+		var sub = $("#sub").val();
+		var msg = $("#msg").val();
+		var to = mailist[current];
+		var data = "ajax=1&from=" + from + "&sub=" + sub + "&msg=" + msg + "&to=" + to;
+			$.ajax({
+				type : 'POST',
+				data:  data,
+				success: function(data) {
+	                $("#result").append(data);
+	            }
+			});
+		}
+
+
+	});
+});
+</script>
 	<!-- Start Customer Reviews -->
 	<div class="customer-reviews-box">
 		<div class="container">
@@ -184,7 +293,7 @@
 						<div class="carousel-inner mt-4">
 							<div class="carousel-item text-center active">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/profile-1.jpg" alt="">
+									<img class="d-block w-100 rounded-circle" src="../images/profile-1.jpg" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Paul Mitchel</strong></h5>
 								<h6 class="text-dark m-0">Web Developer</h6>
@@ -192,7 +301,7 @@
 							</div>
 							<div class="carousel-item text-center">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/profile-3.jpg" alt="">
+									<img class="d-block w-100 rounded-circle" src="../images/profile-3.jpg" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Steve Fonsi</strong></h5>
 								<h6 class="text-dark m-0">Web Designer</h6>
@@ -200,7 +309,7 @@
 							</div>
 							<div class="carousel-item text-center">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/profile-7.jpg" alt="">
+									<img class="d-block w-100 rounded-circle" src="../images/profile-7.jpg" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Daniel vebar</strong></h5>
 								<h6 class="text-dark m-0">Seo Analyst</h6>
@@ -221,7 +330,7 @@
 		</div>
 	</div>
 	<!-- End Customer Reviews -->
-	
+
 	<!-- Start Contact info -->
 	<div class="contact-imfo-box">
 		<div class="container">
@@ -240,6 +349,7 @@
 					<div class="overflow-hidden">
 						<h4>Email</h4>
 						<p class="lead">
+
 							yourmail@gmail.com
 						</p>
 					</div>
@@ -257,7 +367,7 @@
 		</div>
 	</div>
 	<!-- End Contact info -->
-	
+
 	<!-- Start Footer -->
 	<footer class="footer-area bg-f">
 		<div class="container">
@@ -298,7 +408,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="copyright">
 			<div class="container">
 				<div class="row">
@@ -309,23 +419,27 @@
 				</div>
 			</div>
 		</div>
-		
+
 	</footer>
 	<!-- End Footer -->
-	
+
 	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
 
 	<!-- ALL JS FILES -->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="../js/jquery-3.2.1.min.js"></script>
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
     <!-- ALL PLUGINS -->
-	<script src="js/jquery.superslides.min.js"></script>
-	<script src="js/images-loded.min.js"></script>
-	<script src="js/isotope.min.js"></script>
-	<script src="js/baguetteBox.min.js"></script>
-	<script src="js/form-validator.min.js"></script>
-    <script src="js/contact-form-script.js"></script>
-    <script src="js/custom.js"></script>
+	<script src="../js/jquery.superslides.min.js"></script>
+	<script src="../js/images-loded.min.js"></script>
+	<script src="../js/isotope.min.js"></script>
+	<script src="../js/baguetteBox.min.js"></script>
+	<script src="../js/picker.js"></script>
+	<script src="../js/picker.date.js"></script>
+	<script src="../js/picker.time.js"></script>
+	<script src="../js/legacy.js"></script>
+	<script src="../js/form-validator.min.js"></script>
+    <script src="../js/contact-form-script.js"></script>
+    <script src="../js/custom.js"></script>
 </body>
-</html>
+</html> 
