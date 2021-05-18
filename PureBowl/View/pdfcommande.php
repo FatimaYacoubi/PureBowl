c33adc67c1c73c4a79dfb58494c224dd8b6cd937
FatimaYacoubi <yacoubi.fatima@esprit.tn>
2021-05-18 21:19:37 +0200

    Update

--- PureBowl/View/pdfcommande.php
+++ PureBowl/View/pdfcommande.php
@@ -3,12 +3,16 @@ require('../vendor/autoload.php');
 $con=mysqli_connect('localhost','root','','webprojet');
 $res=mysqli_query($con,"select * from commande  ");
 if(mysqli_num_rows($res)>0){
+
 	$html.='<style type="text/css">
 .myOtherTable { background-color:#eedfca;border-collapse:collapse;color:#000;font-size:14px; }
 .myOtherTable th { background-color:#d0a772;color:white;width:10%; border: 1px solid #fff;
             border-collapse: collapse; }
 .myOtherTable td, .myOtherTable th { padding:1px;border: 1px solid #fff; }
 </style>';
+    $html.='<div class="container" align="center"> <br> ';
+
+$html.=' <div class="container" align="center"> <h1> Your Orders </h1>';
 	$html.='<table  class="myOtherTable" border="1" style="border-collapse:collapse ;
  border: 3px solid black;
  width: 700px;
@@ -24,6 +28,7 @@ if(mysqli_num_rows($res)>0){
 	$html.='</table>';
 }else{
 	$html="Data not found";
+	$html.='</div>';
 }
 $mpdf=new \Mpdf\Mpdf();
 $mpdf->WriteHTML($html);
