d400df2f81b3707b01cdb4b806a4f1fed4763ba3
azizchrif <mohamedaziz.cherif@esprit.tn>
2021-05-22 08:58:04 +0100

    Update

--- PureBowl/View/displayRecipe.php
+++ PureBowl/View/displayRecipe.php
@@ -354,9 +354,9 @@
       </div>
     </footer>
 
-    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="../js/jquery-3.3.1.min.js"></script>
     <!-- https://jquery.com/download/ -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
     <!-- https://getbootstrap.com/ -->
     <script>
       $(function() {
